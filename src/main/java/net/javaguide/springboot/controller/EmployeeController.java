6e561a21af5e075ee69395af5d1e731b854b5d53
Thushani <9thushani7@gmail.com>
2022-05-18 15:08:21 +0530

    2022/05/18

--- src/main/java/net/javaguide/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguide/springboot/controller/EmployeeController.java
@@ -17,7 +17,7 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
 
     @GetMapping
-//     This is call as a restAPI
+    // This is call as a restAPI
     public List<Employee> getAllEmployee(){
         return employeeRepository.findAll();
     }
@@ -29,7 +29,8 @@ public class EmployeeController {
     }
 
     // Build get employee by id REST API
-    public ResponseEntity<Employee> getEmployeeById (long id) {
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById( @PathVariable long id) {
         try {
             Employee employee = employeeRepository.findById(id).get();
             return ResponseEntity.ok(employee);
@@ -37,5 +38,22 @@ public class EmployeeController {
             e.printStackTrace();
         }
         return null;
+    }
+
+    //Build update employee REST API
+    @PutMapping("{id}")
+    public  ResponseEntity<Employee> updateEmployee(@PathVariable long id, @RequestBody Employee employeeDetails){
+        // Retrieved employee object from the database using id
+        Employee updateEmployee = employeeRepository.findById(id).get();
+        // update the employee details using received details from client side
+        updateEmployee.setFirstName(employeeDetails.getFirstName());
+        updateEmployee.setLastName(employeeDetails.getLastName());
+        updateEmployee.setEmailId(employeeDetails.getEmailId());
+
+        //Save updated employee data in database
+        employeeRepository.save(updateEmployee);
+
+        // Return updated value to the UI
+        return ResponseEntity.ok(updateEmployee);
     }
 }
